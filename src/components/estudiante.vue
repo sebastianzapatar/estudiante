2363dd50430931a83a2608bbcd8b510e31191885
John Doe <sebastian.zapatar@utadeo.edu.co>
2020-07-03 18:26:08 -0500

    Create estudiante.vue

---
+++ src/components/estudiante.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <Slider texto="Detalles del estudiante"></Slider>
+    </div>
+</template>
+<script>
+import Slider from "./Slider.vue";
+import axios from "axios";
+import Global from "../Global";
+
+export default {
+    name:"Estudiante",
+}
+</script>
